25fc4316eb01a88dbc22a6060c4bc68f9ba87e3e
Rory Primrose <rory@neovolve.com>
2017-10-07 21:45:30 +1100

    Refactored profile display properties onto models. Added copy constructors to models. Updated profileService to use copy constructors. Added initial profile view.

---
+++ client/src/pages/profile/profile.ts
@@ -0,0 +1,78 @@
+import Vue from "vue";
+import Component from "vue-class-component";
+import SkillDetails from "../../controls/skillDetails/skillDetails.vue";
+import { IProfileService, ProfileService } from "../../services/api/profileService";
+import UserProfile from "../../services/api/userProfile";
+import Failure from "../../services/failure";
+import { INotify, Notify } from "../../services/notify";
+import marked from "marked";
+
+@Component({
+    components: {
+      SkillDetails
+    }
+  })
+export default class Profile extends Vue {
+    private profileService: IProfileService;
+    private notify: INotify;
+
+    // Properties for view binding
+    private loading: boolean = true;
+    private compiledMarkdown: string = "";
+    private model: UserProfile = new UserProfile();
+
+    public constructor() {
+        super();
+        
+        this.profileService = new ProfileService();
+        this.notify = new Notify();
+    }
+    
+    public configure(profileService: IProfileService, notify: INotify) {
+        this.profileService = profileService;
+        this.notify = notify;
+    }
+
+    public mounted(): Promise<void> {
+        return this.OnLoad();
+    }
+
+    public async OnLoad(): Promise<void> {
+        this.model = await this.loadProfile(this.$route.params.id);
+
+        this.loading = false;
+    }
+
+    public ShowWebsite(uri: string): void {
+        window.open(uri, '_blank');
+    }
+
+    public CompileMarkdown(): void {
+        if (!this.model.about) {
+            this.compiledMarkdown = "";
+        }
+        else {
+            let options = {
+                sanitized: true
+            };
+    
+            this.compiledMarkdown = marked(this.model.about, options);
+        }
+    }
+    
+    private async loadProfile(id: string): Promise<UserProfile> {
+        try {
+            return await this.profileService.getProfile(id);
+        }
+        catch (failure) {
+            // Check Failure.visibleToUser
+            if (failure.visibleToUser) {
+                this.notify.showFailure(<Failure>failure);
+            } else {
+                throw failure;
+            }
+        }
+
+        return new UserProfile();
+    }
+}
